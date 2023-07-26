5e2aab473f1da5b3697b7572a360bf6863de1346
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2023-07-26 22:12:42 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,77 @@
 "use strict";
 
 // Selecting elements
+const modal = document.getElementById("myModal");
+const slides = document.getElementsByClassName("mySlides");
+const dots = document.getElementsByClassName("demo");
 
-// TODO Open modal/lightbox functionality
+// TODO --------------------------- Open modal/lightbox functionality
+
+// Open the Modal
+function openModal() {
+  modal.style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+let slideIndex = 1; // Sets default slide to be displayed
+
+// Call showSlides with default slide index to display first slide
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  // Loop through slides and hide them
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  // Loop through dots and remove active class
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  // Display current slide and add active class to corresponding dot
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
 
 // TODO Add to cart functionality
-const minusButton = document.getElementById("minus");
-const plusButton = document.getElementById("plus");
-const inputField = document.getElementById("input");
+// const minusButton = document.getElementById("minus");
+// const plusButton = document.getElementById("plus");
+// const inputField = document.getElementById("input");
 
-minusButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  const currentValue = Number(inputField.value) || 0;
-  inputField.value = currentValue - 1;
-});
+// minusButton.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   const currentValue = Number(inputField.value) || 0;
+//   inputField.value = currentValue - 1;
+// });
 
-plusButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  const currentValue = Number(inputField.value) || 0;
-  inputField.value = currentValue + 1;
-});
+// plusButton.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   const currentValue = Number(inputField.value) || 0;
+//   inputField.value = currentValue + 1;
+// });
 
 /*
 Your users should be able to:
