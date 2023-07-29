191f1d74004ffcdb98f5c7969aeea55a9da9a56d
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2023-07-29 15:30:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -88,6 +88,7 @@ function showSlides(n) {
 // });
 
 /*
+TODO Remaining Functionality
 Your users should be able to:
 - View the optimal layout for the site depending on their device's screen size
 - See hover states for all interactive elements on the page
