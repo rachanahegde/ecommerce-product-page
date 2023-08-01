d238a3725424c511d02f5737ad7da7a031e828c9
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2023-08-01 14:48:26 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -222,13 +222,3 @@ cart.addEventListener("click", (event) => {
     cartContainer.style.display = "";
   }
 });
-
-// ---------------------------------------------------------------------------
-
-/*
-TODO Remaining Functionality
-Your users should be able to:
-- View the optimal layout for the site depending on their device's screen size
-- See hover states for all interactive elements on the page
-
-*/
