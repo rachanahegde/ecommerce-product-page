2b97d46f3bf6e8dd239bc4b4da7f9ae4231c368a
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2023-08-01 14:15:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -232,7 +232,5 @@ Your users should be able to:
 - See hover states for all interactive elements on the page
 - Open a lightbox gallery by clicking on the large product image
 - Switch the large product image by clicking on the small thumbnail images
-- Add items to the cart
-- View the cart and remove items from it
 
 */
