e2eb782f277f24f6b68b0271da5249390bfdef9e
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2023-08-01 14:29:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -230,7 +230,5 @@ TODO Remaining Functionality
 Your users should be able to:
 - View the optimal layout for the site depending on their device's screen size
 - See hover states for all interactive elements on the page
-- Open a lightbox gallery by clicking on the large product image
-- Switch the large product image by clicking on the small thumbnail images
 
 */
